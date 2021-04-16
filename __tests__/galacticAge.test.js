6425dfdff5a69e006fbbc556726a5df128093433
aGhouzlane <ahmedghouzlane@gmail.com>
2021-04-16 13:51:25 -0400

    test creation of object of Type GalacticAge and galactic.age Passing

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -4,6 +4,6 @@ import GalacticAge from '../src/js/galacticAge.js';
 describe('GlacticAge', () => {
   test('Should create a new object of Type GalacticAge with parameter age', () => {
     let galacticAge = new GalacticAge(40);
-    expect(galacticAge.age).toEqual(30);
+    expect(galacticAge.age).toEqual(40);
   });
 });
